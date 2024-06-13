b1d960b3f5135f0762a05f0f142769ad75117d5a
noguchi-yu <hnrnyk.12c5@gmail.com>
2024-06-13 21:12:23 +0900

    ログアウトリンク修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  add_flash_types :success, :danger
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -8,5 +9,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar])
     # /users/account_update
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
+    # /users/sign_in
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :avatar])
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  # デバイスのエラーメッセージ出力メソッド
+  def devise_error_messages
+    return "" if resource.errors.empty?
+    html = ""
+    # エラーメッセージ用のHTMLを生成
+    messages = resource.errors.full_messages.each do |msg|
+      html += <<-EOF
+        <div role="alert" class="alert alert-info text-neutral-600 my-1">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+          <span>#{msg}</span>
+        </div>
+      EOF
+    end
+    html.html_safe
+  end
 end
