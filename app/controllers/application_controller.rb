5c6e22a99e2b8b5229a251f5c79603fd35f50205
noguchi-yu <hnrnyk.12c5@gmail.com>
2024-06-14 00:59:19 +0900

    add:avaterのためcarrierwave導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,12 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [ :email, :password, :password_confirmation, :name, :avatar ]
+    added_attrs = [ :email, :password, :password_confirmation, :name, :avatar, :avatar_cache]
     # /users/sign_up
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar, :avatar_cache])
     # /users/account_update
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar, :avatar_cache])
     # /users/sign_in
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :avatar])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :avatar, :avatar_cache])
   end
 end
