30030b30b1d262315405e4a08262493c3ec2da49
noguchi-yu <hnrnyk.12c5@gmail.com>
2024-06-16 15:51:53 +0900

    add:北部九州の住所のチェック

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,14 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 300 }
   validates :address, presence: true, uniqueness: true
+
+  validate :check_address
+
+  # 北部九州のスポットかのチェック
+  def check_address
+    northern_kyushu_addresses = ["福岡県", "佐賀県", "長崎県", "熊本県", "大分県"]
+    unless northern_kyushu_addresses.include?(self.address)
+      errors.add(:base, "は北部九州の住所を入力してください")
+    end
+  end
 end
