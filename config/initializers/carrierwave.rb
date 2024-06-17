342635e16b255c377e0e15028f60b624a94ee5f5
noguchi-yu <hnrnyk.12c5@gmail.com>
2024-06-18 04:25:18 +0900

    update:本番環境で画像がS3で保存されるように変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,12 +5,12 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'バケット名' # バケット名
+    config.fog_directory  = 'kyushu.travel'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数
-      region: ENV['AWS_REGION'], # リージョン
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1',
       path_style: true
     }
 end
